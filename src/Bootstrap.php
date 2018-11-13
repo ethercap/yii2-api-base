e0c2321613492321602823b0d43fd80313d9727f
cuizhihua <cuizhihua@ethercap.com>
2018-11-13 10:40:34 +0800

    s

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -10,5 +10,12 @@ class Bootstrap implements BootstrapInterface
     public function bootstrap($app)
     {
         Yii::$classMap['yii\base\Model'] = '@ethercap/apiBase/components/Model.php';
+        spl_autoload_unregister(['Yii', 'autoload']);
+        spl_autoload_register(['Yii', 'autoload']);
+    }
+
+    public static function map()
+    {
+        return ['yii\base\Model' => '@ethercap/apiBase/components/Model.php'];
     }
 }
--- src/components/ResBuilder.php
+++ src/components/ResBuilder.php
@@ -4,6 +4,7 @@ namespace ethercap\apiBase\components;
 
 use Yii;
 use yii\base\Component;
+use yii\base\Model;
 use yii\web\Response;
 use ethercap\apiBase\components\json\Formatter;
 
@@ -30,6 +31,9 @@ class ResBuilder extends Component
 
     public function run()
     {
+        print_r(Yii::$classMap[Model::class]);
+        print_r(Model::rules());
+        exit;
         Yii::$app->response->formatters[Response::FORMAT_JSON] = Formatter::class;
         $res = $this->template;
         array_walk_recursive($res, function (&$v) {
