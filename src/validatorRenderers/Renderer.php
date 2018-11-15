f97fcedc9f6d25f9330187282071e7f52bd87e48
cuizhihua <cuizhihua@ethercap.com>
2018-11-15 11:17:49 +0800

    添加dict

--- src/validatorRenderers/Renderer.php
+++ src/validatorRenderers/Renderer.php
@@ -4,16 +4,17 @@ namespace ethercap\apiBase\validatorRenderers;
 
 use yii\base\Component;
 use yii\captcha\CaptchaValidator;
+use yii\validators\DefaultValueValidator;
 use yii\validators\EachValidator;
 use yii\validators\ExistValidator;
-use yii\validators\Validator;
-use yii\validators\DefaultValueValidator;
 use yii\validators\FileValidator;
 use yii\validators\FilterValidator;
 use yii\validators\ImageValidator;
 use yii\validators\InlineValidator;
+use yii\validators\RangeValidator;
 use yii\validators\SafeValidator;
 use yii\validators\UniqueValidator;
+use yii\validators\Validator;
 
 class Renderer extends Component
 {
@@ -26,41 +27,49 @@ class Renderer extends Component
     public static function render($validator, $model, $field)
     {
         if ($validator instanceof InlineValidator
-        || $validator instanceof FilterValidator
-        || $validator instanceof FileValidator
-        || $validator instanceof ImageValidator
-        || $validator instanceof SafeValidator
-        || $validator instanceof UniqueValidator
-        || $validator instanceof DefaultValueValidator
-        || $validator instanceof EachValidator
-        || $validator instanceof ExistValidator
-        || $validator instanceof CaptchaValidator
+            || $validator instanceof FilterValidator
+            || $validator instanceof FileValidator
+            || $validator instanceof ImageValidator
+            || $validator instanceof SafeValidator
+            || $validator instanceof UniqueValidator
+            || $validator instanceof DefaultValueValidator
+            || $validator instanceof EachValidator
+            || $validator instanceof ExistValidator
+            || $validator instanceof CaptchaValidator
         ) {
             return [];
         }
-        return [
+        $res = [
             'type' => self::getType($validator),
             'options' => $validator->getClientOptions($model, $field),
         ];
+        if ($validator instanceof RangeValidator) {
+            if (method_exists($model, 'dict') && $dict = $model->dict($field)) {
+                $res['dict'] = $dict;
+            } else {
+                $res['dict'] = array_combine($res['options']['range'], $res['options']['range']);
+            }
+        }
+        return $res;
     }
 
     /**
      * @param $validator
      * @return string
      */
-    public static function getType($validator): string
+    public static function getType($validator)
     {
         if ($type = array_search(get_class($validator), Validator::$builtInValidators)) {
             return $type;
         }
         if ($validator) {
             $type = strtolower(
-            str_replace('Validator', '',
-                substr(get_class($validator),
-                    strrpos(get_class($validator), '\\') + 1
+                str_replace('Validator', '',
+                    substr(get_class($validator),
+                        strrpos(get_class($validator), '\\') + 1
+                    )
                 )
-            )
-        );
+            );
         }
         return $type;
     }
